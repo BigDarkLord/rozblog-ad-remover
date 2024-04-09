c4f49d7903f7cc6fd6467cc3c87d08a42cd7b0f8
BigDarkLord <148328599+BigDarkLord@users.noreply.github.com>
2024-04-09 09:04:13 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+    MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(function(mutations, observer) {
+    mutations?.forEach(function(m){
+    	if(m.target.id?.includes("kaprila")) {
+       	 m.target.remove()
+         console.log(m)
+        }
+    })
+});
+observer.observe(document, {
+  subtree: true,
+  childList: true
+});
